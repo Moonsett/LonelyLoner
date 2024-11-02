f04d0eb30078d6c92ea66d58c79f81408f004cb9
misterj05 <44005965+misterj05@users.noreply.github.com>
2024-11-02 15:15:18 -0400

    rewrite game_worldenv for new TimeAPI

--- LonelyLoner/Scenes/Map/game_worldenv.gd
+++ LonelyLoner/Scenes/Map/game_worldenv.gd
@@ -1,6 +1,6 @@
 extends WorldEnvironment
 
-### Get OS time every 15 minutes, then check time range to see what color des_color should be
+### Check time every physics frame, then check time range to see what color des_color should be
 ### Figure out a way to do algebraic math on hex colors, maybe convert them to vectors and do math on those?
 ### Maybe figure out good ambient lighting, too, idk (#ffa370 Sunset, #1a1426 Night, #d5eeff Day)
 
@@ -9,16 +9,13 @@ var des_fog_end = 500
 var des_color = Color("#ffa370")
 var des_light = 0.1
 var rain = false
-var ingame_used_time = Time.get_datetime_dict_from_system()
-var current_os_time = null
+onready var time = TimeAPI.check_time()
 
 var default_fog = 125
 var default_fog_end = 500
 
-# Note for Firefly stuff: This is most certainly NOT how you do this!! Rewrite this later
-
 func _set_color_by_time():
-	match ingame_used_time["hour"]:
+	match time["hour"]:
 		0, 1, 2, 3, 4, 20, 21, 22, 23:
 			des_color = Color("#14253e")
 		5, 6, 7:
@@ -29,20 +26,17 @@ func _set_color_by_time():
 			des_color = Color("#d5eeff")
 		_:
 			des_color = Color("d5eeff")
+			print(str(self) + ": WHERE ARE YOU?!?! (could not set time by hour because hour is either overflowed or doesn't exist)")
 
 func _ready():
 	PlayerData.connect("_rain_toggle", self, "_rain_env")
 	OptionsMenu.connect("_options_update", self, "_dist_update")
 	_dist_update()
-	_set_color_by_time()
-
-func _check_time_deviation():
-	current_os_time = Time.get_datetime_dict_from_system()
-	if current_os_time["hour"] != ingame_used_time["hour"]:
-		ingame_used_time = Time.get_datetime_dict_from_system()
-		_set_color_by_time()
 
 func _physics_process(delta):
+	time = TimeAPI.check_time()
+	_set_color_by_time()
+	
 	var lerp_val = 0.01
 	
 	environment.fog_depth_begin = lerp(environment.fog_depth_begin, des_fog, lerp_val)
@@ -51,8 +45,6 @@ func _physics_process(delta):
 	environment.background_color = lerp(environment.background_color, des_color, lerp_val)
 	environment.ambient_light_color = lerp(environment.ambient_light_color, des_color, lerp_val)
 	environment.fog_color = lerp(environment.fog_color, des_color, lerp_val)
-	
-	_check_time_deviation()
 	
 	$DirectionalLight.light_energy = lerp($DirectionalLight.light_energy, des_light, lerp_val)
 
