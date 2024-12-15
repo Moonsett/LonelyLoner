1b9263a6a3a79d35ecd97d6ac2617cfbbf07830e
misterj05 <44005965+misterj05@users.noreply.github.com>
2024-12-15 05:54:42 -0500

    massive rewrite

--- LonelyLoner/mods/LonelyLoner/fireflies.gd
+++ LonelyLoner/mods/LonelyLoner/fireflies.gd
@@ -1,8 +1,9 @@
 extends Spatial
 
-onready var time = TimeAPI.check_time()
+onready var LonelyLoner = get_node_or_null("/root/LonelyLoner")
+onready var time = LonelyLoner.check_time()
 
-func _set_visibility_by_time():
+func _load():
 	match time["hour"]:
 		0, 1, 2, 3, 4, 20, 21, 22, 23:
 			visible = true
@@ -10,15 +11,30 @@ func _set_visibility_by_time():
 			visible = false
 		17, 18, 19:
 			visible = true
-		8, 9, 10, 11, 12, 13, 14, 15, 16:
+		5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16:
 			visible = false
+
+func _set_visibility_by_time():
+	time = LonelyLoner.check_time()
+	match time["hour"]:
+		0, 1, 2, 3, 4, 20, 21, 22, 23:
+			$Fading.play("FadeIn")
+			if visible == false: visible = true
+		5, 6, 7:
+			$Fading.play("FadeOut")
+		17, 18, 19:
+			$Fading.play("FadeIn")
+			if visible == false: visible = true
+		5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16:
+			$Fading.play("FadeOut")
 		_:
 			visible = false
 			print(str(self) + ": WHERE ARE YOU?!?! (could not set visibility by hour because hour is either overflowed or doesn't exist)")
 
 func _ready():
-	pass
+	_load()
+	LonelyLoner.connect("hour_has_passed", self, "_set_visibility_by_time")
 
-func _process(delta):
-	time = TimeAPI.check_time()
-	_set_visibility_by_time()
+func _physics_process(delta):
+	pass
+	#_set_visibility_by_time()
--- LonelyLoner/mods/LonelyLoner/main.gd
+++ LonelyLoner/mods/LonelyLoner/main.gd
@@ -1,28 +1,83 @@
 extends Node
 
-const ID = "LonelyLonerTimeAPI"
-onready var real_time = {"hour": 0, "minute": 0, "second": 0}
-onready var ingame_time = {"hour": 0, "minute": 0, "second": 0}
+signal second_has_passed
+signal minute_has_passed
+signal hour_has_passed
+signal day_has_passed
 
-var min_timer = null
-var sec_timer = null
-var in_game_sec_timer = null
-var lh_timer = null
-var mode = "RealTime"
+const ID = "LonelyLoner"
+onready var real_time = {"hour": 0, "minute": 0, "second": 0}
+onready var ingame_time = {"hour": 12, "minute": 30, "second": 0}
+
+onready var worldenv:WorldEnvironment = get_node_or_null("/root/world/Viewport/main/map/main_map/WorldEnvironment")
+onready var main_zone = get_node_or_null("/root/world/Viewport/main/map/main_map/zones/main_zone")
+
+var LL_fireflies = preload("res://mods/LonelyLoner/Assets/Scenes/fireflies.tscn").instance()
+var LL_campfire = preload("res://mods/LonelyLoner/Assets/Scenes/campfire.tscn").instance()
+
+var des_color = null
+var min_timer:Timer
+var sec_timer:Timer
+var in_game_sec_timer:Timer
+var lh_timer:Timer
+var mode = "IngameTime"
 var check_lh = false
-var ingame_second_length = 0.1
+var ingame_second_length = 0.0000000001
+var fireflies_added = false
 
 func _ready():
 	print(ID + " has loaded!")
+	_startup()
+
+func _startup():
 	check_time()
 	match mode:
 		"RealTime":
 			create_timer(min_timer, 60, "check_time") # Set the interval at which to poll irl time
-			create_timer(sec_timer, 1, "_poll_long_haul")
+			#create_timer(sec_timer, 1, "_poll_long_haul")
 		"IngameTime":
 			create_timer(min_timer, ingame_second_length, "check_time") # Set the interval at which to poll in game time
-			create_timer(sec_timer, 1, "_poll_long_haul")
+			#create_timer(sec_timer, 1, "_poll_long_haul")
 			create_timer(in_game_sec_timer, ingame_second_length, "_in_game_time_has_passed")
+
+func _cleanup():
+	main_zone.remove_child(LL_fireflies)
+	main_zone = null
+	fireflies_added = false
+
+func _node_scanner():
+	if worldenv != null:
+		worldenv.connect("tree_exiting", self, "_cleanup")
+	if worldenv != null:
+		_set_color_by_time()
+	#if is_instance_valid(fireflies) == false:
+	#	fireflies = load("res://mods/LonelyLoner/Assets/Scenes/fireflies.tscn").instance()
+	if main_zone != null && fireflies_added == false:
+		#print("Tried to add fireflies")
+		main_zone.add_child(LL_fireflies)
+		main_zone.connect("tree_exiting", self, "_cleanup")
+	else:
+		worldenv = get_node_or_null("/root/world/Viewport/main/map/main_map/WorldEnvironment")
+		main_zone = get_node_or_null("/root/world/Viewport/main/map/main_map/zones/main_zone")
+
+func _physics_process(delta):
+	_node_scanner()
+	print(ingame_time)
+
+func _set_color_by_time():
+	var time = check_time()
+	match time["hour"]:
+		0, 1, 2, 3, 4, 20, 21, 22, 23:
+			worldenv.des_color = Color("#14253e")
+		5, 6, 7:
+			worldenv.des_color = Color("#ffd6e7")
+		17, 18, 19:
+			worldenv.des_color = Color ("#ffa370")
+		8, 9, 10, 11, 12, 13, 14, 15, 16:
+			worldenv.des_color = Color("#d5eeff")
+		_:
+			worldenv.des_color = Color("d5eeff")
+			print(str(self) + ": WHERE ARE YOU?!?! (could not set time by hour because hour is either overflowed or doesn't exist)")
 
 func create_timer(timer, wait_by, function):
 	timer = Timer.new()
@@ -34,23 +89,27 @@ func create_timer(timer, wait_by, function):
 
 func _in_game_time_has_passed():
 	ingame_time["second"] = ingame_time["second"] + 1
+	emit_signal("second_has_passed")
 	if ingame_time["second"] >= 60:
 		ingame_time["second"] = 0
 		ingame_time["minute"] = ingame_time["minute"] + 1
+		emit_signal("minute_has_passed")
 	if ingame_time["minute"] >= 60:
 		ingame_time["minute"] = 0
 		ingame_time["hour"] = ingame_time["hour"] + 1
+		emit_signal("hour_has_passed")
 	if ingame_time["hour"] >= 24:
 		ingame_time = {"hour": 0, "minute": 0, "second": 0}
+		emit_signal("day_has_passed")
 
 func check_time():
 	match mode:
 		"RealTime":
 			real_time = Time.get_time_dict_from_system()
-#			print(ID + ": " + str(real_time)) # Just for debug to make sure it be working
+			#print(ID + ": " + str(real_time)) # Just for debug to make sure it be working
 			return real_time
 		"IngameTime":
-#			print(ID + ": " + str(ingame_time)) # Just for debug to make sure it be working
+			#print(ID + ": " + str(ingame_time)) # Just for debug to make sure it be working
 			return ingame_time
 
 # checks every second to see if it should switch to polling at a set interval
